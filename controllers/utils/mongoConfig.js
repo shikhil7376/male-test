5bc9bd801469d0b6800fcfeee7663a6d7c901aeb
shikhil <shikhilkrishnan2018@gmail.com>
2024-09-04 14:35:14 +0530

    done changes in ui of dashboard

--- controllers/utils/mongoConfig.js
+++ controllers/utils/mongoConfig.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 function connectDB() {
   mongoose
-    .connect(process.env.MONGODB)
+    .connect('mongodb://127.0.0.1:27017/MALE_FASHION', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("DATABASE CONNECTED"))
     .catch((error) => console.log(error));
 }
