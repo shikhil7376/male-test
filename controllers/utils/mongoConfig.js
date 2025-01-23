8280629aab4a76ecb5c2c84729ea8b1b13cbd3d5
shikhil <shikhilkrishnan2018@gmail.com>
2025-01-23 20:27:49 +0530

    added the atlas url

--- controllers/utils/mongoConfig.js
+++ controllers/utils/mongoConfig.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 function connectDB() {
   mongoose
-    .connect('mongodb://127.0.0.1:27017/MALE_FASHION', { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("DATABASE CONNECTED"))
     .catch((error) => console.log(error));
 }
